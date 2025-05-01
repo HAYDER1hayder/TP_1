910da8870a9083ddc4289c0d5844ddc8651024a7
HAYDER1hayder <hayderkays30@gmail.com>
2025-05-01 15:30:50 +0100

    Update Employe.java

--- src/dz/univeloued/tps/classes/Employe.java
+++ src/dz/univeloued/tps/classes/Employe.java
@@ -1,22 +1,25 @@
 package dz.univeloued.tps.classes;
 
-public class Employe extends Personne {
+public class Employe extends Personne {// inheritance 
     protected double salaire;
 
+    // Constructor avec parameters
     public  Employe(int id, String prenom,String nom, double salaire) {
         super(id, nom, prenom);
         this.salaire = salaire;
     }
 
+    //Methods Get
     public double getSalaire() {
         return salaire;
     }
 
+    //Methods Set
     public void setSalaire(double salaire) {
         this.salaire = salaire;
     }
 
-    @Override
+    // Override
     public String toString() {
         return "Je suis " + prenom.toUpperCase() + " " + nom + " mon salaire est: " + salaire + " DA";
     }
